d5f1210d2d18b82ce2d9adeb703d9341207c4aff
deleiy <derlei1.vanderlei@gmail.com>
2024-07-24 23:12:37 -0300

    opção de saída
    incluso no sistema a opção de saída para o usuário

--- cartório deley banco de dados ,1.c
+++ cartório deley banco de dados ,1.c
@@ -130,6 +130,7 @@ int main()
 	printf("\t1 - Registra nomes\n");
 	printf("\t2 - consultar nomes\n");
 	printf("\t3 - Deletar nomes\n\n");
+	printf("\t4 - Sair do sistema\n\n");
 	printf("opção:"); // fim do menu
 	
 	scanf("%d",  &opcao); //Armazenado a escolha do usuário
@@ -151,7 +152,10 @@ int main()
             deletar(); // deletar nomes 	
          	break;	
          	
-         	
+         	case 4:
+         	printf("Obrigado por utilizar o sistema!\n");	
+         	return 0;
+         	break;
 	    
 	    	default:
 		    printf("Essa opção não está disponivel!\n");	
